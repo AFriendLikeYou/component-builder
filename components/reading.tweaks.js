4ed2a87264cba309d1d9a2489abb03243684bca2
AFriendLikeYou <mail@a-friend-like-you.de>
2026-09-25 16:45:08 +0200

    Feinschliff: reading

--- components/reading.tweaks.js
+++ components/reading.tweaks.js
@@ -33,6 +33,18 @@ Factory.tweaks("reading", {
           "flexDirection": "column"
         }
       }
+    },
+    "text": {
+      "text:artikel>0": {
+        "style": "anzeige",
+        "css": {
+          "fontFamily": "\"Inter\", sans-serif",
+          "fontSize": "64px",
+          "lineHeight": "64px",
+          "fontWeight": "400",
+          "fontStyle": "normal"
+        }
+      }
     }
   }
 });
